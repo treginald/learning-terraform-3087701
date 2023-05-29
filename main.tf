aa09dae6810867b8f7faa9c48b66aaea0ca52dfc
treginald <101239748+treginald@users.noreply.github.com>
2023-05-29 21:38:05 +0930

    dev vpc

--- main.tf
+++ main.tf
@@ -16,6 +16,7 @@ data "aws_ami" "app_ami" {
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
+  version = "3.14.2"
 
   name = "dev"
   cidr = "10.0.0.0/16"
@@ -23,7 +24,7 @@ module "vpc" {
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  #enable_nat_gateway = true
+  enable_nat_gateway = true
 
   tags = {
     Terraform = "true"
@@ -47,7 +48,7 @@ module "web_sg" {
   version = "4.17.2"
   name = "web_new"
 
-  vpc_id = module.vpc.public_subnets[1]
+  vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
